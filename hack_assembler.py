0d826707213e6bf1f515c8c9966d27881aaa94b3
CodeQuiver <38484031+CodeQuiver@users.noreply.github.com>
2024-10-23 13:49:15 -0400

    first commit

---
+++ hack_assembler.py
@@ -0,0 +1,265 @@
+"""
+Accepts argument of path to Prog.asm, containing a valid Hack Assembly Language program.
+Translates from Hack Assembly Language to Hack binary code.
+Stores resulting binary in Prog.hack, in the same folder as the source,
+overwriting if necessary.
+"""
+
+import re
+import sys
+import os
+
+# Constants- rule dicts representing tables
+
+destination_dict = {
+    None: "000",
+    "M": "001",
+    "D": "010",
+    "DM": "011",
+    "MD": "011",
+    "A": "100",
+    "AM": "101",
+    "MA": "101",
+    "AD": "110",
+    "DA": "110",
+    "ADM": "111",
+    "AMD": "111",
+    "MDA": "111",
+    "MAD": "111",
+    "DAM": "111",
+    "DMA": "111",
+}
+
+jump_dict = {
+    None: "000",
+    "JGT": "001",
+    "JEQ": "010",
+    "JGE": "011",
+    "JLT": "100",
+    "JNE": "101",
+    "JLE": "110",
+    "JMP": "111",
+}
+
+comp_dict = {
+    "0": "0101010",
+    "1": "0111111",
+    "-1": "111010",
+    "D": "0001100",
+    "A": "0110000",
+    "!D": "0001101",
+    "!A": "0110001",
+    "-D": "0001111",
+    "-A": "0110011",
+    "D+1": "0011111",
+    "A+1": "0110111",
+    "D-1": "0001110",
+    "A-1": "0110010",
+    "D+A": "0000010",
+    "D-A": "0010011",
+    "A-D": "0000111",
+    "D&A": "0000000",
+    "D|A": "0010101",
+    "M": "1110000",
+    "!M": "1110001",
+    "-M": "1110011",
+    "M+1": "1110111",
+    "M-1": "1110010",
+    "D+M": "1000010",
+    "D-M": "1010011",
+    "M-D": "1000111",
+    "D&M": "1000000",
+    "D|M": "1010101",
+}
+
+# Symbol Table is below- will be dynamically added to
+# during label and variable handling
+# see pg. 82 in book for specification
+# "symbol":"decimal value",
+# Note- first address for Variable symbols will be
+# RAM 16
+symbol_table_dict = {
+    "R0": 0,
+    "SP": 0,
+    "R1": 1,
+    "LCL": 1,
+    "R2": 2,
+    "ARG": 2,
+    "R3": 3,
+    "THIS": 3,
+    "R4": 4,
+    "THAT": 4,
+    "R5": 5,
+    "R6": 6,
+    "R7": 7,
+    "R8": 8,
+    "R9": 9,
+    "R10": 10,
+    "R11": 11,
+    "R12": 12,
+    "R13": 13,
+    "R14": 14,
+    "R15": 15,
+    "SCREEN": 16384,
+    "KBD": 24576,
+}
+
+
+def a_instruction(instruction: str):
+    """If line is an a-instruction, call this function.
+
+    Args:
+        instruction (str): individual instruction to encode
+
+    Returns:
+        string: 16-bit machine code representation of instruction
+    """
+    # 1- Set first bit to 0
+    # 2- if command is a decimal value, translate to binary using 15 bits
+    # 3- if command is a symbol/variable, just return the 0 and nothing else for now
+    command = instruction[1:]
+
+    if not command.isdecimal():
+        try:
+            command = symbol_table_dict[command]
+            # fetch decimal encoding
+        except KeyError:
+            raise KeyError(
+                f"{command} not found in symbol table, a-instruction '{instruction}' can't be translated"
+            )
+
+    binary_str = "{0:015b}".format(int(command))
+    return f"0{binary_str}"
+
+
+def destination(dest_command: str):
+    try:
+        return destination_dict[dest_command]
+    except KeyError:
+        raise KeyError(f"{dest_command} not found in destination table")
+
+
+def jump(jump_command: str):
+    try:
+        return jump_dict[jump_command]
+    except KeyError:
+        raise KeyError(f"{jump_command} not found in jump dict")
+
+
+def computation(comp_command: str):
+    try:
+        return comp_dict[comp_command]
+    except KeyError:
+        raise KeyError(f"{comp_command} not found in computation dict")
+
+
+def c_instruction(instruction: str):
+    """
+    If line is an c-instruction, call this function. Outputs 16-bit machine code representation.
+    """
+    # separate into "computation", "destination", and "jump" fields
+    #
+    # syntax:
+    # dest = comp ; jump
+    #
+    # encode each field separately
+    # recombine them in order
+    # 1 1 1 a c1 c2 c3 c4 c5 c6 d1 d2 d3 j1 j2 j3
+    # ("a" is part of computation fields)
+
+    # default values
+    dest_code = "000"
+    comp_code = "0000000"
+    jump_code = "000"
+
+    # case 1: dest = comp ; jump
+    if ("=" in instruction) and (";" in instruction):
+        instruction_list = re.split("=|;", instruction)
+
+        dest_code = destination(instruction_list[0])
+        comp_code = computation(instruction_list[1])
+        jump_code = jump(instruction_list[2])
+
+    # case 2: dest = comp
+    elif ("=" in instruction) and (";" not in instruction):
+        instruction_list = re.split("=", instruction)
+
+        dest_code = destination(instruction_list[0])
+        comp_code = computation(instruction_list[1])
+
+    # case 3: comp ; jump
+    elif (";" in instruction) and ("=" not in instruction):
+        instruction_list = re.split(";", instruction)
+
+        comp_code = computation(instruction_list[0])
+        jump_code = jump(instruction_list[1])
+
+    else:
+        raise ValueError(
+            f"c-instruction '{instruction}' lacks required syntax markers of either '=' or ';', cannot be assembled"
+        )
+
+    result = f"111{comp_code}{dest_code}{jump_code}"
+
+    # return final binary as string
+    return result
+
+
+def main(path: str):
+    """
+        1- read file at given path line-by-line
+        2- remove comments and whitespace
+        3- assemble into list of commands- input_lines_original
+        4- deal with symbols/variable substitution- return input_lines_working
+        5- parse result of step 4 - process each command
+        6- insert translation of commands into output_binary list
+        7- write output_binary list line-by-line to Prog.hack at same path as input file
+        8- also return output_binary or a success/failure message to be output to console
+
+    Args:
+        path (string): Path to <input>.asm, containing a valid Hack Assembly Language program.
+    """
+    output_binary = []
+
+    print("PATH: " + path)
+
+    with open(path, mode="r") as file:
+        # Syntax Notes:
+        # `re.sub(r"\s+", "", line)` is for removing all whitespaces- strip only trims leading and trailing
+        clean_lines = [
+            re.sub(r"\s+|//.*$", "", line)
+            for line in file
+            if line and (not line.isspace()) and (not line.startswith("//"))
+        ]
+
+        # print("CLEAN LINES: " + str(clean_lines))
+
+        for line in clean_lines:
+            if line.startswith("@"):
+                output_line = a_instruction(line)
+                output_binary.append(output_line)
+            elif line.startswith("("):
+                # ignore label lines
+                continue
+            else:
+                # must be c-instruction
+                output_line = c_instruction(line)
+                output_binary.append(output_line)
+
+    # print("output_binary: " + str(output_binary))
+
+    path_parent_directory = os.path.dirname(path)  # get path only- cuts off filename
+
+    with open(f"{path_parent_directory}/Prog.hack", "w") as output_file:
+        for line in output_binary:
+            output_file.write(line + "\n")
+
+    return output_binary
+
+
+if __name__ == "__main__":
+    result = main(sys.argv[1])
+
+    # print any return values to console
+    if result:
+        print(result)
---
+++ test/test_hack_assembler.py
@@ -0,0 +1,253 @@
+import hack_assembler
+import IPython  # use pytest -v -s to eanble IPython embed debugging within a test
+
+# command-line tests to copy-paste:
+# python hack_assembler/hack_assembler.py "hack_assembler/translation_target/Add.asm"
+
+
+class TestAInstruction:
+
+    def test_decimal_1(self):
+        assert hack_assembler.a_instruction("@1") == "0" * 15 + "1"
+
+    def test_decimal_2(self):
+        assert hack_assembler.a_instruction("@2") == "0" * 14 + "10"
+
+    def test_keyboard(self):
+        """should return 0 + the address for KBD which is 24576 or 110 0000 0000 0000"""
+        assert hack_assembler.a_instruction("@KBD") == "0110000000000000"
+
+
+class TestCInstruction:
+    # Encodings:
+    # 1 1 1 a c1 c2 c3 c4 c5 c6 d1 d2 d3 j1 j2 j3
+    def test_d_plus_a(self):
+        """
+        "D=D+A": "1110000010010000",
+        """
+        assert hack_assembler.c_instruction("D=D+A") == "1110000010010000"
+
+    def test_m_eq_neg_1(self):
+        """
+        "M=-1: should encode to "1110111010001000"
+        """
+        assert hack_assembler.c_instruction("M=-1") == "1110111010001000"
+        # TODO- fix this test failing
+
+    def test_md_eq_m_plus_1(self):
+        """
+        "MD=M+1" should encode to "1111110111011000"
+        """
+        assert hack_assembler.c_instruction("MD=M+1") == "1111110111011000"
+
+
+class TestHackAssemblerNoSymbols:
+    # To run just this class- syntax
+    #  pytest hack_assembler/test/test_hack_assembler.py::TestHackAssembler
+    def test_add(self):
+        result_add = hack_assembler.main("hack_assembler/translation_target/Add.asm")
+        assert isinstance(result_add, list)
+        assert len(result_add) == 6
+        # result as code: ["@2", "D=A", "@3", "D=D+A", "@0", "M=D"]
+
+        expected_result = [
+            "0000000000000010",
+            "1110110000010000",
+            "0000000000000011",
+            "1110000010010000",
+            "0000000000000000",
+            "1110001100001000",
+        ]
+
+        for i, expected in enumerate(expected_result):
+            assert (
+                expected == result_add[i]
+            ), f"line {i} expected {expected} but got actual {result_add[i]}"
+
+    def test_maxl(self):
+        # ['@0', 'D=M', '@1', 'D=D-M', '@10', 'D;JGT', '@1', 'D=M', '@12', '0;JMP', '@0', 'D=M', '@2', 'M=D', '@14', '0;JMP']
+        # Encodings:
+        # 1 1 1 a c1 c2 c3 c4 c5 c6 d1 d2 d3 j1 j2 j3
+        # IPython.embed()
+        # import ipdb
+
+        # ipdb.set_trace()
+
+        result_max = hack_assembler.main("hack_assembler/translation_target/MaxL.asm")
+        assert isinstance(result_max, list)
+
+        assert len(result_max) == 16
+
+        assert result_max[0] == "0" * 16  # '@0'  # first line "@0" should be all zeroes
+        assert result_max[1] == "1111110000010000"  # 'D=M'
+        assert result_max[2] == "0" * 15 + "1"
+        assert result_max[3] == "1111010011010000"  # 'D=D-M'
+        assert result_max[4] == "0" * 12 + "1010"  # '@10'
+        assert result_max[5] == "1110001100000001"  # 'D;JGT' comp;jump
+        assert result_max[6] == "0" * 15 + "1"  # @1
+        assert result_max[7] == "1111110000010000"  # dest=010  #comp = 1110000 # 'D=M'
+        assert result_max[8] == "0" * 12 + "1100"  # '@12'
+        assert result_max[9] == "1110101010000111"  # '0;JMP'
+        assert result_max[10] == "0" * 16  # '@0'
+        assert result_max[11] == "1111110000010000"  # 'D=M'
+        assert result_max[12] == "0" * 14 + "10"  # '@2'
+        assert result_max[13] == "1110001100001000"  # dest=001  # comp=0001100  # 'M=D'
+        assert result_max[14] == "0" * 12 + "1110"  # '@14'
+        assert result_max[15] == "1110101010000111"  # '0;JMP'
+
+        expected_output = [
+            "0000000000000000",
+            "1111110000010000",
+            "0000000000000001",
+            "1111010011010000",
+            "0000000000001010",
+            "1110001100000001",
+            "0000000000000001",
+            "1111110000010000",
+            "0000000000001100",
+            "1110101010000111",
+            "0000000000000000",
+            "1111110000010000",
+            "0000000000000010",
+            "1110001100001000",
+            "0000000000001110",
+            "1110101010000111",
+        ]
+
+        for i, expected in enumerate(expected_output):
+            assert (
+                expected == result_max[i]
+            ), f"line {i} expected {expected} but got actual {result_max[i]}"
+
+    def test_output_written_in_file_add(self):
+        result_add = hack_assembler.main("hack_assembler/translation_target/Add.asm")
+        assert isinstance(result_add, list)
+        assert len(result_add) == 6
+        # result as code: ["@2", "D=A", "@3", "D=D+A", "@0", "M=D"]
+        assert result_add == [
+            "0000000000000010",
+            "1110110000010000",
+            "0000000000000011",
+            "1110000010010000",
+            "0000000000000000",
+            "1110001100001000",
+        ]
+
+        with open("hack_assembler/translation_target/Prog.hack", "r") as output_file:
+            for i, line in enumerate(output_file):
+                assert (
+                    line.removesuffix("\n") == result_add[i]
+                ), f"line {i} expected {result_add[i]} but got actual {line.removesuffix("\n")}"
+
+    def test_rectl(self):
+        hack_assembler.main("hack_assembler/translation_target/RectL.asm")
+
+        output_file = open("hack_assembler/translation_target/Prog.hack", "r")
+        output_lines = output_file.readlines()
+        output_file.close()
+
+        with open(
+            "hack_assembler/translation_target/Rect_test_expected.hack", "r"
+        ) as expected_output_file:
+            expected_output_lines = expected_output_file.readlines()
+            assert len(output_lines) == len(expected_output_lines)
+
+            for i, actual_line in enumerate(output_lines):
+                actual = actual_line.removesuffix("\n")
+                expected = expected_output_lines[i].removesuffix("\n")
+                assert (
+                    actual == expected
+                ), f"line {i} expected {expected} but got actual {actual}"
+
+    def test_pongl(self):
+        hack_assembler.main("hack_assembler/translation_target/PongL.asm")
+
+        output_file = open("hack_assembler/translation_target/Prog.hack", "r")
+        output_lines = output_file.readlines()
+        output_file.close()
+
+        with open(
+            "hack_assembler/translation_target/Pong_test_expected.hack", "r"
+        ) as expected_output_file:
+            expected_output_lines = expected_output_file.readlines()
+            assert len(output_lines) == len(expected_output_lines)
+
+            for i, actual_line in enumerate(output_lines):
+                actual = actual_line.removesuffix("\n")
+                expected = expected_output_lines[i].removesuffix("\n")
+                assert (
+                    actual == expected
+                ), f"line {i} expected {expected} but got actual {actual}"
+
+
+class TestHackAssemblerWithSymbols:
+    def test_max(self):
+
+        result_max = hack_assembler.main("hack_assembler/translation_target/Max.asm")
+        assert isinstance(result_max, list)
+
+        assert len(result_max) == 16
+
+        expected_output = [
+            "0000000000000000",
+            "1111110000010000",
+            "0000000000000001",
+            "1111010011010000",
+            "0000000000001010",
+            "1110001100000001",
+            "0000000000000001",
+            "1111110000010000",
+            "0000000000001100",
+            "1110101010000111",
+            "0000000000000000",
+            "1111110000010000",
+            "0000000000000010",
+            "1110001100001000",
+            "0000000000001110",
+            "1110101010000111",
+        ]
+
+        for i, expected in enumerate(expected_output):
+            assert (
+                expected == result_max[i]
+            ), f"line {i} expected {expected} but got actual {result_max[i]}"
+
+    def test_rect(self):
+        hack_assembler.main("hack_assembler/translation_target/Rect.asm")
+
+        output_file = open("hack_assembler/translation_target/Prog.hack", "r")
+        output_lines = output_file.readlines()
+        output_file.close()
+
+        with open(
+            "hack_assembler/translation_target/Rect_test_expected.hack", "r"
+        ) as expected_output_file:
+            expected_output_lines = expected_output_file.readlines()
+            assert len(output_lines) == len(expected_output_lines)
+
+            for i, actual_line in enumerate(output_lines):
+                actual = actual_line.removesuffix("\n")
+                expected = expected_output_lines[i].removesuffix("\n")
+                assert (
+                    actual == expected
+                ), f"line {i} expected {expected} but got actual {actual}"
+
+    def test_pong(self):
+        hack_assembler.main("hack_assembler/translation_target/Pong.asm")
+
+        output_file = open("hack_assembler/translation_target/Prog.hack", "r")
+        output_lines = output_file.readlines()
+        output_file.close()
+
+        with open(
+            "hack_assembler/translation_target/Pong_test_expected.hack", "r"
+        ) as expected_output_file:
+            expected_output_lines = expected_output_file.readlines()
+            assert len(output_lines) == len(expected_output_lines)
+
+            for i, actual_line in enumerate(output_lines):
+                actual = actual_line.removesuffix("\n")
+                expected = expected_output_lines[i].removesuffix("\n")
+                assert (
+                    actual == expected
+                ), f"line {i} expected {expected} but got actual {actual}"
